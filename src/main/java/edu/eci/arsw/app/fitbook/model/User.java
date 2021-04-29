a0ad27d1c26f432d475dd12457ed1c67dd314f43
SoyTiyi <santiago.marmartinez@gmail.com>
2021-04-29 07:59:29 -0500

    fix user entity

--- src/main/java/edu/eci/arsw/app/fitbook/model/User.java
+++ src/main/java/edu/eci/arsw/app/fitbook/model/User.java
@@ -18,7 +18,7 @@ public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @Column(name = "userid")
+    @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int user_id;
 
