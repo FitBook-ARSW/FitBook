7bde725b965c1adab2b25aa44d488e163c6aff00
SoyTiyi <santiago.marmartinez@gmail.com>
2021-05-19 14:24:35 -0500

    santiago version

--- src/main/java/edu/eci/arsw/app/fitbook/persistence/impl/AgendaPersistence.java
+++ src/main/java/edu/eci/arsw/app/fitbook/persistence/impl/AgendaPersistence.java
@@ -5,11 +5,13 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import edu.eci.arsw.app.fitbook.model.Agenda;
+import edu.eci.arsw.app.fitbook.model.Class;
 import edu.eci.arsw.app.fitbook.persistence.FitBookPersistenceException;
 import edu.eci.arsw.app.fitbook.persistence.IAgendaPersistence;
 import edu.eci.arsw.app.fitbook.persistence.repo.IAgendaRepo;
@@ -24,9 +26,17 @@ public class AgendaPersistence implements IAgendaPersistence{
     EntityManager entityManager;
 
     @Override
+    @Transactional
     public void addAgenda(Agenda agenda) throws FitBookPersistenceException {
         try {
             ar.save(agenda);
+            Query querySecond = entityManager.createNativeQuery("select * from clase where id=?", Class.class);
+            querySecond.setParameter(1, agenda.getIdclase());
+            Class clase = (Class) querySecond.getSingleResult();
+            clase.setMax(clase.getMax()-1);
+            Query queryThird = entityManager.createNativeQuery("update clase set max=? where id=?");
+            queryThird.setParameter(1, clase.getMax());
+            queryThird.setParameter(2, clase.getId());
         } catch (Exception e) {
             throw new FitBookPersistenceException("Error to save a new class");
         }
